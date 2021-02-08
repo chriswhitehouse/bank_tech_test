0d8232e243d40a7922abe745229fa41fc0a21431
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-08 14:04:22 +0000

    implement a withdrawal unit test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,4 +8,10 @@ describe Account do
       expect(account.deposit(1000)).to eq 1000
     end
   end
+
+  describe "#withdrawal" do
+    it 'should remove money from the account balance' do
+      expect(account.withdrawal(1000)).to eq 1000
+    end
+  end
 end
