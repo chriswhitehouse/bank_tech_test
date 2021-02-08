7a7597e76ff69e34c5cd24862e0c7d6fa043a89c
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-08 14:09:06 +0000

    Implement print_statement unit test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,7 +1,9 @@
 require 'account.rb'
+require 'time'
 
 describe Account do
   let(:account) { Account.new }
+  let(:test_date) { Date.new(2012,01,12) }
 
   describe "#deposit" do
     it 'should add money to the account balance' do
@@ -12,6 +14,14 @@ describe Account do
   describe "#withdrawal" do
     it 'should remove money from the account balance' do
       expect(account.withdrawal(1000)).to eq 1000
+    end
+  end
+
+  describe "#print_statement" do
+    it 'should print a list of transactions and the account balance' do
+      allow(Date).to receive(:today).and_return(test_date)
+      expect(account.print_statement).to eq "date || credit || debit || balance\n
+      12/01/2012 || 1000.00 || || 1000.00"
     end
   end
 end
