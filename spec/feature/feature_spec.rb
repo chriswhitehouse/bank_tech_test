fcaeed33626ec4f7cd2168bf163af40926329af1
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-02-09 16:30:28 +0000

    Refactor feature test set up out of helper files and back into spec file

--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,14 +1,27 @@
 # frozen_string_literal: true
 
 require 'time'
-require './spec/helpers/transaction_helper'
-require './spec/helpers/statement_helper'
 
 describe 'Acceptance Criteria' do
   let(:account) { Account.new }
+  let(:testdate1) { Date.new(2012, 1, 12) }
+  let(:testdate2) { Date.new(2012, 1, 13) }
+  let(:testdate3) { Date.new(2012, 1, 14) }
+
+  let(:statement_string) {
+                          "date || credit || debit || balance\n"\
+                          "14/01/2012 || || 500.00 || 2500.00\n"\
+                          "13/01/2012 || 2000.00 || || 3000.00\n"\
+                          "12/01/2012 || 1000.00 || || 1000.00"
+                          }
 
   it 'should receive two deposits and one withdrawal and return a statement' do
-    three_transactions
+    allow(Date).to receive(:today).and_return(testdate1)
+    account.deposit(1000)
+    allow(Date).to receive(:today).and_return(testdate2)
+    account.deposit(2000)
+    allow(Date).to receive(:today).and_return(testdate3)
+    account.withdrawal(500)
 
     expect { account.print_statement }.to output(statement_string).to_stdout
   end
